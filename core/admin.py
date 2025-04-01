d50d437d1b9acc2aac422aec95026a0ca50cb123
Dorian Santner <dorian.santner@providens.at>
2025-04-01 14:31:41 +0200

    admin and model update for minidemo

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,14 @@
 # Copyright (c) 2025 Python User Group Graz. Distributed under the MIT license.
 from django.contrib import admin
 
-from core.models import Issue, Label
+from core.models import Issue, Label, Participant
 from minitrack.settings import ADMIN_UI_KIND, AdminUiKind
 
 if ADMIN_UI_KIND == AdminUiKind.BARE_BONES:
     # The bare-bones admin UI automatically generated from the model
     admin.site.register(Issue)
     admin.site.register(Label)
+    admin.site.register(Paticipant)
 
 elif ADMIN_UI_KIND == AdminUiKind.ENHANCED:
     # An admin UI that already provides improved list display, search and sorting.
@@ -23,6 +24,77 @@ elif ADMIN_UI_KIND == AdminUiKind.ENHANCED:
     class LabelAdmin(admin.ModelAdmin):
         ordering = ["title"]
 
+    @admin.register(Participant)
+    class ParticipantAdmin(admin.ModelAdmin):
+        ordering = ["issue", "user", "role"]
+
 elif ADMIN_UI_KIND == AdminUiKind.FANCY:
     # An admin UI with some bells and whistles to show-case what's possible with extensions.
-    pass  # TODO Dorian: Add fancy admin UI.
+
+    # Inlines ---------------------------------------------------------------------------
+    # https://docs.djangoproject.com/en/5.1/ref/contrib/admin/#inlinemodeladmin-objects
+
+    class ParticipantInlineAdmin(admin.StackedInline):
+        model = Participant
+        extra = 0
+        classes = ["collapse"]
+        readonly_fields = ["show"]  # method in class
+        fields = ["user", "role", "show"]
+
+    class IssueInlineAdmin(admin.TabularInline):
+        model = Issue.labels.through
+        extra = 0
+
+    # Filter ----------------------------------------------------------------------------
+
+    class IssueDemoFilter(admin.SimpleListFilter):
+        title = "Active"
+        parameter_name = "active"
+
+        def lookups(self, request, model_admin):
+            # usually : qs = model_admin.get_queryset(request)
+            #           .. handle/filter/aggregate .. return [(attrib,text), ..]
+
+            return [("yes", "Active"), ("no", "Inactive")]
+
+        def queryset(self, request, queryset):
+            if self.value() == "yes":
+                return queryset.filter(state__in=["todo", "doing"])
+            elif self.value() == "no":
+                return queryset.filter(state="done")
+            else:
+                return queryset
+
+    # Admin classes ---------------------------------------------------------------------
+
+    @admin.register(Issue)
+    class IssueAdmin(admin.ModelAdmin):
+        list_display = ("title", "state", "assignee__username")
+        list_filter = ("state", "assignee", IssueDemoFilter)
+        ordering = ["title", "id"]
+        search_fields = (
+            "assignee__first_name",
+            "assignee__username",
+            "description",
+            "title",
+        )
+
+        inlines = [
+            ParticipantInlineAdmin,
+        ]
+        actions = ["demo_action_status_change"]
+
+        @admin.action(description="change selected to 'done'")
+        def demo_action_status_change(self, request, queryset):
+            queryset.update(state="done")
+
+    @admin.register(Label)
+    class LabelAdmin(admin.ModelAdmin):
+        ordering = ["title"]
+        inlines = [
+            IssueInlineAdmin,
+        ]
+
+    @admin.register(Participant)
+    class ParticipantAdmin(admin.ModelAdmin):
+        ordering = ["issue", "user", "role"]
--- core/models.py
+++ core/models.py
@@ -2,11 +2,19 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from django.utils.html import format_html
+
 
 class IssueState(models.TextChoices):
     TODO = "todo"
     DOING = "doing"
     DONE = "done"
+
+
+class ROLE_CHOICES(models.TextChoices):
+    OWNER = "o", "Owner"
+    DEV = "d", "Developer"
+    OTHER = "-", "Other"
 
 
 class Label(models.Model):
@@ -16,9 +24,25 @@ class Label(models.Model):
         return self.title
 
 
+class Participant(models.Model):
+    issue = models.ForeignKey("Issue", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=1, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"Participant {self.id} ({self.role} Issue:{self.issue_id} User:{self.user})"
+
+    def show(self):
+        """show self safe for web"""
+        return format_html(
+            f"<b>{self.user.first_name} {self.user.last_name}</b> has role '<b>{self.get_role_display()}</b>' on Issue '<b>{self.issue}</b>'"
+        )
+
+
 class Issue(models.Model):
     assignee = models.ForeignKey(User, on_delete=models.CASCADE, related_name="issues")
     labels = models.ManyToManyField(Label, related_name="issues")
+    labels2 = models.ManyToManyField(User, through="Participant")
 
     title = models.CharField(max_length=100)
     state = models.CharField(choices=IssueState, default=IssueState.TODO)
